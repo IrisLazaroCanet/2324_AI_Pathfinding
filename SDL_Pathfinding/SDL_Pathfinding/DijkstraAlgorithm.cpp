bba034c4ed47d32e49de46446d1a1d4d7498b9b5
Emma <emma.polanco@enti.cat>
2023-11-06 18:46:31 +0100

    First try - DIJKSTRA

--- SDL_Pathfinding/SDL_Pathfinding/DijkstraAlgorithm.cpp
+++ SDL_Pathfinding/SDL_Pathfinding/DijkstraAlgorithm.cpp
@@ -1,7 +1,68 @@
 #include "DijkstraAlgorithm.h"
 
+
 Path* DijkstraAlgorithm::FindPath(Graph* graph, Node* origin, Node* goal)
 {
     std::cout << "Calculate path using Dijkstra algorithm" << std::endl;
+
+    // Create a priority queue for Dijkstra
+    std::priority_queue<std::pair<float, Node*>, std::vector<std::pair<float, Node*>>, std::greater<std::pair<float, Node*>>> pq;
+
+    // Initialize distance to all nodes as infinity (except for the origin)
+    std::map<Node*, float> distance;
+    distance[origin] = 0.0f;
+
+    // Create a map to keep track of parents for backtracking
+    std::map<Node*, Node*> parentMap;
+
+    // Push the origin node with distance 0 to the priority queue
+    pq.push(std::make_pair(0.0f, origin));
+
+    while (!pq.empty())
+    {
+        Node* current = pq.top().second;
+        float currentDistance = pq.top().first;
+        pq.pop();
+
+        // If the goal node is reached, construct the path and return it
+        if (current == goal)
+        {
+            Path* path = new Path;
+            Node* currentNode = goal;
+
+            // Backtrack from the goal to the origin using parentMap
+            while (currentNode != nullptr)
+            {
+                path->points.insert(path->points.begin(), currentNode->GetCell());
+                currentNode = parentMap[currentNode];
+            }
+
+            return path;
+        }
+
+        // Get the connections of the current node from the graph
+        std::vector<Connection*> connections = graph->GetConnections(current->GetId());
+
+        for (Connection* connection : connections)
+        {
+            Node* neighbor = graph->GetNodeFromId(connection->GetToNode()).second;
+
+            // Calculate the new distance to the neighbor through the current node
+            float newDistance = currentDistance + connection->GetCost();
+
+            // If the new distance is shorter, update it and push to the priority queue
+            if (newDistance < distance[neighbor])
+            {
+                distance[neighbor] = newDistance;
+                parentMap[neighbor] = current;
+                pq.push(std::make_pair(newDistance, neighbor));
+            }
+        }
+    }
+
+    // If no path is found, return nullptr
     return nullptr;
 }
+
+
+
